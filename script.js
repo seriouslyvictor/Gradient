a341e7d9699cc177022abf000e2f579679b40f47
seriouslyvictor <schiavovictor@outlook.com>
2023-11-15 22:39:20 -0300

    added JS functionality

---
+++ script.js
@@ -0,0 +1,26 @@
+const bodyEl = document.querySelector("body");
+const toggleEl = document.querySelector(".icone--tema");
+
+toggleEl.addEventListener("click", trocarTema);
+
+function trocarTema() {
+  bodyEl.classList.toggle("light");
+  atualizarIcone();
+}
+
+function atualizarIcone(lightMode) {
+  toggleEl.src = lightMode ? "img/icon-sun.svg" : "img/icon-moon.svg";
+}
+
+function trocarTimeAuto() {
+  let hora = new Date().getHours();
+  let lightMode = hora >= 6 && hora < 18;
+  console.log(
+    `Disparei a função automática às ${hora} horas e ${new Date().getMinutes()} minutos`
+  );
+  lightMode ? bodyEl.classList.add("light") : bodyEl.classList.remove("light");
+  atualizarIcone(lightMode);
+}
+
+trocarTimeAuto();
+setInterval(trocarTimeAuto, 60000);
